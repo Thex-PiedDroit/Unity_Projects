593ea0fb65785018591b383d61c0ce647720bb60
Maxime Samyn <maxime.samyn@etud.infographie-sup.eu>
2015-10-25 22:20:55 +0100

    Added main menu
    "Select level" currently launches Map_1 directly
    (Can't currently go back to main menu from game)

---
+++ Syndicate_Recreate/Assets/Scripts/Main_Title.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Title : MonoBehaviour
+{
+	#region Variables (public)
+	
+	
+	
+	#endregion
+	
+	#region Variables (private)
+	
+	
+	
+	#endregion
+
+
+	public void LoadMap(string pMapName)
+	{
+		Application.LoadLevel(pMapName);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
---
+++ Syndicate_Recreate/Assets/Scripts/Main_Title_MouseOver.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Title_MouseOver : MonoBehaviour
+{
+	#region Variables (public)
+
+	[SerializeField]
+	private GameObject m_pButtonMouseOverOverlay;
+	[SerializeField]
+	private GameObject m_pButtonClickedOverlay;
+	
+	#endregion
+	
+	#region Variables (private)
+	
+	
+	
+	#endregion
+
+	
+	public void MouseEnter()
+	{
+		m_pButtonMouseOverOverlay.SetActive(true);
+	}
+
+	public void MouseExit()
+	{
+		m_pButtonMouseOverOverlay.SetActive(false);
+		m_pButtonClickedOverlay.SetActive(false);
+	}
+
+	public void MouseDown()
+	{
+		m_pButtonClickedOverlay.SetActive(true);
+	}
+
+	public void MouseUp()
+	{
+		m_pButtonClickedOverlay.SetActive(false);
+	}
+}
